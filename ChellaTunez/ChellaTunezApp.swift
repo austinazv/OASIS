fb8d117f0b85cb1bc3fd59fdc53d91ec5535d299
Austin Zambito-Valente <azv04@stanford.edu>
2025-01-14 22:47:05 -0500

    Initial Commit

---
+++ ChellaTunez/ChellaTunezApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChellaTunezApp.swift
+//  ChellaTunez
+//
+//  Created by Austin Zambito-Valente on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ChellaTunezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
